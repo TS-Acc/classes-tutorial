e356c6f9918251e43b1c7d37529abfdd5cb45b53
Trevor Shepherd <t.rayshpd@gmail.com>
2023-02-13 23:45:15 -0500

    Commiting everything currently in the project

---
+++ class-tutorial/Program.cs
@@ -0,0 +1,64 @@
+﻿using class_tutorial;
+
+Student joe = new Student
+{
+    Id = 1,
+    Firstname = "Joe",
+    Age = 41,
+    IsActive = true,
+    DateCreated = new DateTime(2023, 2, 10)
+};
+
+Student jane = new Student
+{
+    Id = 2,
+    Firstname = "Jane",
+    Age = 31,
+    IsActive = false,
+    DateCreated = new DateTime(2023, 2, 13)
+};
+
+Student pat = new Student(101, "Pat", 22); //initialize an instance using a constructor
+
+Console.WriteLine($"{pat.Id} | {pat.Firstname} | {pat.Age} | {pat.DateCreated}");
+
+Console.WriteLine($"Instance Joe's first name is {joe.Firstname}"); // before executing a method to change Joe's name
+joe.ChangeFirstName("Joseph");                                      // always have parentheses when defining or executing a method
+Console.WriteLine($"Instance Joe's first name is {joe.Firstname}"); // after executing a method to change Joe's name
+
+Console.WriteLine($"Instance Jane's age is {jane.Age}"); // before executing a method to change Jane's age
+jane.IncrementAge();                                     // no parameters were passed to the IncrementAge method when defined so the
+                                                         // parentheses stay empty
+Console.WriteLine($"Instance Jane's age is {jane.Age}"); // after executing a method to change Jane's age
+
+Console.WriteLine($"Instance Jane's date created is {jane.DateCreated}");          // before executing a method to add 30 days to Jane's date created
+var JaneDateCreatedPlus30Days = jane.CalcCreateDatePlus30Days();                   // created a variable to hold the return of the CalcCreateDatePlus30Days method
+Console.WriteLine($"Instance Jane's date created is {JaneDateCreatedPlus30Days}"); // after executing a method to change Jane's date created
+
+
+
+Customer customer1 = new Customer();
+customer1.AccountNumber = "A001";
+customer1.Name = "Alphabet soup";
+customer1.Email = "info@alphabetsoup.com";
+customer1.Phone = "800-555-1212";
+
+Customer customer2 = new Customer {
+    AccountNumber = "B002",
+    Name = "ButterflySongs",
+    Email = "info@butterflysongs.com",
+    Phone = "888-555-2121"
+};
+
+//Console.WriteLine($"Id           : {joe.Id}");
+//Console.WriteLine($"Firstname    : {joe.Firstname}");
+//Console.WriteLine($"Age          : {joe.Age}");
+//Console.WriteLine($"IsActive     : {joe.IsActive}");
+//Console.WriteLine($"DateCreated  : {joe.DateCreated}");
+
+//Console.WriteLine(customer2.Name);
+
+
+
+
+
---
+++ class-tutorial/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_tutorial
+{
+    internal class Student
+    {
+
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public int Age { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public Student() // a default constructor (a constructor that takes no parameters)
+        {
+            Id = -1;
+            Firstname = "NO NAME";
+            Age = -1;
+            DateCreated = new DateTime(2023, 2, 1);
+        }
+
+        public Student(string firstname, int age) // a constructor using parameters
+        {
+            Firstname = firstname;
+            Age = age;
+        }
+
+        public Student(int id, string firstname, int age)
+        {
+            Id = id;
+            Firstname = firstname;
+            Age = age;
+        }
+
+
+        public void IncrementAge()
+        {
+            Age = Age + 1;
+        }
+
+        public void ChangeFirstName(string Firstname)
+        {
+            this.Firstname = Firstname;
+            //   property =  parameter
+            // this. is used to call a propety or method in a class i.e. to distinguish the property Firstname and the parameter Firstname
+        }
+
+        public string CalcCreateDatePlus30Days()
+        {
+           return DateCreated.AddDays(30).ToString("MM/dd/yyyy");
+        }
+
+
+    }
+}
